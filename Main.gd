21a8e11b4a00ce99b61fd2db9e11cc11d65c4531
freddie-a <57713959+freddie-a@users.noreply.github.com>
2022-10-30 08:47:10 +0000

    Remove dead code

--- Main.gd
+++ Main.gd
@@ -1,9 +1,6 @@
 extends Node2D
 
 export(PackedScene) var enemy_scene
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 onready var globals = get_node("/root/Global")
 
 # Called when the node enters the scene tree for the first time.
@@ -24,9 +21,5 @@ func _on_EnemySpawnTimer_timeout():
 	var enemy = enemy_scene.instance()
 	var spawn_loc = get_node("EnemySpawn/EnemySpawnLocation")
 	spawn_loc.offset = randi()
-	#var direction = spawn_loc.rotation + PI / 2
 	enemy.position = spawn_loc.position
-	#enemy.rotation = enemy.position.angle_to_point(globals.playerPos)
-	#var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
-	#enemy.linear_velocity = velocity.rotated(direction)
 	add_child(enemy)
